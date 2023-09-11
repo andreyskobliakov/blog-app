90c6c047cb9481bf08d5f46ac951961d464230ff
k0laedr <andre.oleynik@gmail.com>
2023-09-11 21:22:40 +0300

    fix package

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -19,6 +19,6 @@
 </template>
 
 <script setup>
-import Header from '../components/Header.vue';
-import Footer from '../components/Footer.vue';
+import Header from './Header.vue';
+import Footer from './Footer.vue';
 </script>
